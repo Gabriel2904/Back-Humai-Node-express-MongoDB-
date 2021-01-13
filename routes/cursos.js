c4ceb9a28dd796dfc4554dbf20edaa27aaf99d4e
Gabriel2904 <gabriel2904@hotmail.com>
2021-01-13 08:54:54 -0300

    Rutas+MongoDB

--- routes/cursos.js
+++ routes/cursos.js
@@ -1,10 +1,49 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const model = require("./../models/cursos");
 
+const all = (req, res) =>
+  model
+    .get({ conditions: {}, fields: {} })
+    .then((response) => res.json(response))
+    .catch((e) => res.sendStatus(500));
+const single = (req, res) =>
+  model
+    .findById(req.params.id)
+    .then(([response]) => res.json(response))
+    .catch((e) => res.sendStatus(500));
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const last = (req, res) =>
+  model
+    .last()
+    .then((response) => res.json(response))
+    .catch((e) => res.sendStatus(500));
 
+const test = (req, res) =>
+  model
+    .findByDates(parseInt(req.query.start), parseInt(req.query.end))
+    .then((response) => res.json(response))
+    .catch((e) => res.sendStatus(500));
+/*
+const single = (req, res) =>
+  model
+    .get({
+      conditions: { _id: `ObjectId(${req.params.id})` },
+      fields: { nombre: true, apellido: true },
+    })
+    .then(([response]) => res.json(response))
+    .catch((e) => res.sendStatus(500));
+*/
+const create = (req, res) => {
+  model
+    .create(req.body)
+    .then(({ insertId }) => res.json(insertId))
+    .catch((e) => res.sendStatus(500));
+};
+router.get("/test", test);
+
+router.get("/last", last);
+router.get("/all", all);
+router.get("/single/:id", single);
+router.post("/create", create);
 module.exports = router;
--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,20 @@
-const mongoose = require("mongoose");
+const MongoClient = require("mongodb").MongoClient;
+
+const pool = async () => {
+  try {
+    return (await MongoClient.connect("mongodb://localhost:27017/")).db(
+      "DBHumai"
+    );
+  } catch (err) {
+    console.log(err.stack);
+  }
+  
+};
+module.exports = { pool };
+
+/*const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost/DBHumai")
-
+  .connect("mongodb://localhost:27017/")
+  .then((db) => console.log("DN is conected"))
+  .catch((err) => console.error(err));*/
