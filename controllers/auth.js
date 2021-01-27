5e89dc18721b5cc253f30abb15f70a2d0454ce98
Gabriel2904 <gabriel2904@hotmail.com>
2021-01-27 00:48:17 -0300

    bcryptjs/JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,23 @@
 const User = require("./../models/usuario");
 const { hash, unhash } = require("./../utils/bcrypts");
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
+const privateKey = fs.readFileSync("./keys/private.pem");
 
 const signUp = async (req, res) => {
   try {
     const { username, email, password, roles } = req.body;
+    const userFound = User.find({ email });
     const newUser = new User({
       username,
       email,
       password: hash(password),
     });
-    await newUser.save();
-    console.log(newUser);
-    res.json("signUp");
+    const savedUser = await newUser.save();
+    const token = jwt.sign({ id: savedUser._id }, privateKey, {
+      expiresIn: 3600,
+    });
+    res.json({ token });
   } catch (e) {
     console.error(e);
     res.status(500).json({ ok: false, e });
